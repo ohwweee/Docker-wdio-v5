34521552be2ae2bdf430a454ad3e93449f055223
ashaporov <AShaporov@gk-software.com>
2019-07-26 11:47:32 +0300

    upd to v5

--- wdio-e2e/specs/main.spec.ts
+++ wdio-e2e/specs/main.spec.ts
@@ -1,18 +1,16 @@
-import { expect, assert } from "chai";
-import * as webdriverio from "webdriverio";
-import * as mocha from "mocha";
+import { assert } from "chai";
 
-const protocol = "http"
-const host = process.env.APP_HOST || "localhost";
-const port = "3000";
-const toolUrl = `${protocol}://${host}:${port}`;
+// const protocol = "http"
+// const host = process.env.APP_HOST || "localhost";
+// const port = "3000";
+// const toolUrl = `${protocol}://${host}:${port}`;
 
 
-describe('local docker test', function () {
+describe('local docker test', () => {
    
-    it('page should be loaded', function () {
-        browser.url('http://frontend')
-        let title = $('body > app-root > h1')
-        assert.isTrue(title.getText().includes('wdio-frontend'))
+    it('page should be loaded', () => {
+        browser.url('http://frontend');
+        let title = $('body > app-root > h1');
+        assert.isTrue(title.getText().includes('wdio-frontend'));
     })
 });
